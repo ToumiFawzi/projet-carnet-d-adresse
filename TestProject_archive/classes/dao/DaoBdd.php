e11b860669bd4047bec10ddd41767874416f460f
Ftoumi <fawzi.t@hotmail.fr>
2018-03-05 11:16:23 +0100

    maj

---
+++ TestProject_archive/classes/dao/DaoBdd.php
@@ -0,0 +1,18 @@
+<?php
+namespace dao;
+
+class DaoBdd
+{
+
+    protected $bdd;
+
+    public function __construct($config)
+    {
+        $this->bdd = new \PDO("mysql:host=" . $config['db.host'] . ";dbname=" . $config['db.name'] . ";charset=utf8",
+            $config['db.username'], 
+            $config['db.password']);
+    }
+}
+    
+
+
